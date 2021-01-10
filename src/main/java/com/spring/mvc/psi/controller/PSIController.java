fa12773224e3bcc0cf4a751e8f0cef03d87af018
foxlo <foxlo@DESKTOP-43M7EB4>
2021-01-10 16:17:25 +0800

    SpringData - psi -case

--- src/main/java/com/spring/mvc/psi/controller/PSIController.java
+++ src/main/java/com/spring/mvc/psi/controller/PSIController.java
@@ -1,6 +1,6 @@
 package com.spring.mvc.psi.controller;
 
-
+import java.util.Optional;
 import com.spring.mvc.psi.entities.Product;
 import com.spring.mvc.psi.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +9,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/psi")
@@ -21,11 +23,27 @@ public class PSIController {
     ProductRepository productRepository;
     
     // 讀取商品資料
-    @GetMapping(value = {"/product"})
-    public String readProduct(Model model){
+    @GetMapping(value = {"/product", "/product/{id}", "/product/{delete}/{id}"})
+    public String readProduct(  Model model, 
+                                @RequestParam Optional<Boolean> deleteError,
+                                @PathVariable Optional<Integer> id,
+                                @PathVariable Optional<String> delete ){
         Product product = new Product();
+        String _method = "POST";
+        
+        if(id.isPresent()) {
+            product = productRepository.findOne(id.get());
+            if(delete.isPresent() && delete.get().equals("delete")) {
+                _method = "DELETE";
+            } else {
+                _method = "PUT";
+            }
+        }
+        
+        model.addAttribute("_method", _method);
         model.addAttribute("product", product);
         model.addAttribute("products", productRepository.findAll());
+        model.addAttribute("deleteError", deleteError.isPresent()?"刪除失敗":"");
         return "psi/product";
     }
     
@@ -43,6 +61,11 @@ public class PSIController {
         return "redirect: ../psi/product";
     }
     
-    
+    // 刪除商品
+    @DeleteMapping(value = {"/product"})
+    public String deleteProduct(@ModelAttribute("product") Product product) {
+        productRepository.delete(product.getId());
+        return "redirect: ../psi/product";
+    }
     
 }
